4d25446c7143bc3ffaa5c608b3412494436c927b
crsgyj <crsgyj@aliyun.com>
2020-07-06 00:30:34 +0800

    init

---
+++ session.go
@@ -0,0 +1,161 @@
+package opst
+
+import (
+  "bytes"
+  "fmt"
+  "time"
+
+  //"os"
+  "strings"
+
+  //"github.com/spf13/cobra/cobra/cmd"
+  "golang.org/x/crypto/ssh"
+  "net"
+  "os/exec"
+)
+
+type Session interface {
+  Run(cmd string) error
+  Close() error
+  Output() string
+  ErrMsg() string
+}
+
+type SessionConfig struct {
+  User     string
+  Password string
+  Host     string
+  Port     int8
+  Timeout  int
+}
+
+type localSession struct {
+  cmd    *exec.Cmd
+  msgBuf *bytes.Buffer
+  errBuf *bytes.Buffer
+}
+
+type remoteSession struct {
+  session *ssh.Session
+  msgBuf  *bytes.Buffer
+  errBuf  *bytes.Buffer
+}
+
+var TerminalSessions = struct {
+  Remote func(config SessionConfig) func() (Session, error)
+  Local  func() func() (Session, error)
+}{
+  Remote: newRemoteSession,
+  Local:  newLocalSession,
+}
+
+func newRemoteSession(config SessionConfig) func() (Session, error) {
+  return func() (Session, error) {
+    r := remoteSession{
+      msgBuf: &bytes.Buffer{},
+      errBuf: &bytes.Buffer{},
+    }
+    session, err := sshConnect(config.Host, config.Port, config.User, config.Password, config.Timeout)
+    if err != nil {
+      return nil, err
+    }
+    r.session = session
+    r.session.Stdout = r.msgBuf
+    r.session.Stderr = r.errBuf
+    return r, nil
+  }
+}
+
+func newLocalSession() func() (Session, error) {
+  return func() (Session, error) {
+    r := localSession{
+      cmd:    &exec.Cmd{},
+      msgBuf: &bytes.Buffer{},
+      errBuf: &bytes.Buffer{},
+    }
+
+    r.cmd.Stdout = r.msgBuf
+    r.cmd.Stderr = r.errBuf
+    return r, nil
+  }
+}
+
+func (r localSession) Run(command string) error {
+  args := strings.Split(strings.TrimSpace(command), " ")
+  for i, _ := range args {
+    args[i] = strings.TrimSpace(args[i])
+  }
+  r.cmd.Path = args[0]
+  r.cmd.Args = args
+  err := r.cmd.Run()
+  if err != nil {
+    r.errBuf.WriteString(err.Error())
+  }
+  return err
+}
+func (r localSession) Close() error {
+  return nil
+}
+func (r localSession) Output() string {
+  return r.msgBuf.String()
+}
+func (r localSession) ErrMsg() string {
+  return r.errBuf.String()
+}
+
+func (r remoteSession) Run(command string) error {
+  err := r.session.Run(command)
+  if err != nil {
+    r.errBuf.WriteString(err.Error())
+  }
+  return err
+}
+
+func (r remoteSession) Close() error {
+  return r.session.Close()
+}
+func (r remoteSession) Output() string {
+  return r.msgBuf.String()
+}
+func (r remoteSession) ErrMsg() string {
+  return r.errBuf.String()
+}
+
+func sshConnect(host string, port int8, user, password string, timeout int) (*ssh.Session, error) {
+  var (
+    auth         []ssh.AuthMethod
+    addr         string
+    clientConfig *ssh.ClientConfig
+    client       *ssh.Client
+    session      *ssh.Session
+    err          error
+  )
+  if timeout == 0 {
+    timeout = 20000
+  }
+  // get auth method
+  auth = make([]ssh.AuthMethod, 0)
+  auth = append(auth, ssh.Password(password))
+
+  hostKeyCallbk := func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+    return nil
+  }
+
+  clientConfig = &ssh.ClientConfig{
+    User:            user,
+    Auth:            auth,
+    Timeout:         time.Duration(timeout) * time.Second,
+    HostKeyCallback: hostKeyCallbk,
+  }
+
+  addr = fmt.Sprintf("%s:%d", host, port)
+
+  if client, err = ssh.Dial("tcp", addr, clientConfig); err != nil {
+    return nil, err
+  }
+  // create session
+  if session, err = client.NewSession(); err != nil {
+    return nil, err
+  }
+  return session, nil
+}
---
+++ task_test.go
@@ -0,0 +1,51 @@
+package opst
+
+import (
+  "testing"
+)
+
+func TestTask_Abandon(t *testing.T) {
+  task := Task{
+    Name:    "go env",
+    Command: "go env",
+    Session: TerminalSessions.Local(),
+  }
+  task.Abandon()
+  if !task.abandon {
+    t.Fail()
+  }
+}
+
+func TestTask_SetState(t *testing.T) {
+  var (
+    payload = make(map[string]interface{})
+    task    = Task{
+      Name:       "TEST setState",
+      Command:    "command1",
+      Session:    nil,
+      payload:    &payload,
+      AllowError: false,
+      Async:      false,
+      completed:  false,
+      Init:       nil,
+      Done:       nil,
+    }
+    kv = map[string]interface{}{
+      "keyA": "valueA",
+      "keyB": struct{}{},
+      "keyC": nil,
+      "keyD": 1,
+    }
+  )
+
+  for k, v := range kv {
+    task.SetState(k, v)
+    if getV := task.GetState(k); getV != v {
+      goto ERR
+    }
+  }
+
+  return
+ERR:
+  t.Fail()
+}
